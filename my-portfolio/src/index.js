359947e0ca13e431d8083e3a3b04aa7fdfc002f9
Marcel0906 <m.kubetin89@gmail.com>
2024-01-26 11:47:52 +0100

    index.js update

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
-
 
